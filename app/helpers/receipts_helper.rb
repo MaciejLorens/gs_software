07b809bde62f4ba1a7ddf53ebb035db010fa383e
Maciej Lorens <maciej.lorens@gmail.com>
2018-12-01 18:02:37 +0100

    layout for drivers

--- app/helpers/receipts_helper.rb
+++ app/helpers/receipts_helper.rb
@@ -1,9 +1,18 @@
 module ReceiptsHelper
-  def index_active
+
+  def index_action?
+    controller.action_name == 'index'
+  end
+
+  def new_action?
+    controller.action_name == 'new'
+  end
+
+  def index_active?
     controller.action_name == 'index' ? 'active' : ''
   end
 
-  def new_active
+  def new_active?
     controller.action_name == 'new' ? 'active' : ''
   end
 
@@ -14,7 +23,7 @@ module ReceiptsHelper
   end
 
   def options_for_drivers
-    current_company.users.drivers.map do |driver|
+    current_company.drivers.map do |driver|
       ["#{driver.first_name} #{driver.last_name}", driver.id, {class: 'dropdown-item'}]
     end
   end

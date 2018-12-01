8d577aefcb459dc236e1f784259f2e00e7a58211
Maciej Lorens <maciej.lorens@gmail.com>
2018-12-01 12:33:53 +0100

    new receipt form

--- app/helpers/receipts_helper.rb
+++ app/helpers/receipts_helper.rb
@@ -1,2 +1,9 @@
 module ReceiptsHelper
+  def index_active
+    controller.action_name == 'index' ? 'active' : ''
+  end
+
+  def new_active
+    controller.action_name == 'new' ? 'active' : ''
+  end
 end

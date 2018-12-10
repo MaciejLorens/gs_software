3aab47b5b31e32642d350fa23010972c9bc75708
Maciej Lorens <maciej.lorens@gmail.com>
2018-12-10 19:51:03 +0100

    rebuild qrcode structure

--- app/models/notify.rb
+++ app/models/notify.rb
@@ -11,24 +11,40 @@ class Notify < ApplicationRecord
 
   def qr_code_string
     {
-      id: id,
-      pin: pin,
-      number: number,
-      car_number: car_number,
-      semitrailer_number: semitrailer_number,
-      expiration_from: expiration_from.to_i,
-      indefinite: indefinite,
-      expiration_to: expiration_to.to_i,
-      hidden: hidden,
-      hidden_at: hidden_at.to_i,
-      product_id: product_id,
-      product_name: product.name,
-      driver_id: driver_id,
-      driver_full_name: driver.try(:full_name),
-      active: active,
-      created_at: created_at.to_i,
-      company_id: company_id,
-      company_name: company.name
+      notify: {
+        id: id,
+        pin: pin,
+        number: number,
+        car_number: car_number,
+        semitrailer_number: semitrailer_number,
+        expiration_from: expiration_from.to_i,
+        expiration_to: expiration_to.to_i,
+        indefinite: indefinite,
+        hidden: hidden,
+        hidden_at: hidden_at.to_i,
+        active: active,
+        updated_at: updated_at.to_i,
+        created_at: created_at.to_i,
+      },
+      creator: {
+        id: user.id,
+        email: user.email,
+        first_name: user.first_name,
+        last_name: user.last_name
+      },
+      product: {
+        id: product_id,
+        name: product.name
+      },
+      driver: {
+        id: driver_id,
+        first_name: driver.first_name,
+        last_name: driver.last_name
+      },
+      company: {
+        id: company_id,
+        name: company.name
+      }
     }.to_json.to_s
   end
 
